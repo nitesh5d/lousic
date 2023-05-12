1d0d78cf152859b1f4095949674e65f3cb31f7d0
Nitesh <niteshgupta288@gmail.com>
2023-05-12 19:23:33 +0530

    help center design done

---
+++ app/src/main/java/in/fivedegree/lousic/SignUpHelpBox.kt
@@ -0,0 +1,22 @@
+package `in`.fivedegree.lousic
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class SignUpHelpBox : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+
+
+
+        return inflater.inflate(R.layout.fragment_sign_up_help_box, container, false)
+    }
+}
