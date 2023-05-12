726d123a4cb4ac1d6fedf3578d2f6d2f1233cf30
Nitesh <niteshgupta288@gmail.com>
2023-05-12 19:19:49 +0530

    Bottom Nav done on MainActivity

--- app/src/main/java/in/fivedegree/lousic/MainActivity.kt
+++ app/src/main/java/in/fivedegree/lousic/MainActivity.kt
@@ -1,21 +1,71 @@
 package `in`.fivedegree.lousic
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlin.math.sign
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    private lateinit var dp: ImageView
+    private lateinit var name: TextView
+    private lateinit var uid: TextView
+    private lateinit var bnv: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = Firebase.auth
+
+        bnv = findViewById(R.id.bottom_navigation)
+
+//        dp = findViewById(R.id.dp)
+//        name = findViewById(R.id.name)
+//        uid = findViewById(R.id.uid)
+//        val db = DBHelper(this, null)
+//        val cursor = db.getName()
+//        cursor!!.moveToFirst()
+//        name.text = cursor.getString(cursor.getColumnIndex(DBHelper.NAME_COl))
+//        uid.text = cursor.getString(cursor.getColumnIndex(DBHelper.UID))
+//        cursor.close()
+
+        bnv.itemIconTintList = null
+        loadFragment(ExploreFragment())
+        bnv.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> {
+                    loadFragment(ExploreFragment())
+                    true
+                }
+                R.id.upload -> {
+                    loadFragment(UploadFragment())
+                    true
+                }
+                R.id.profile -> {
+                    loadFragment(ProfileFragment())
+                    true
+                }
+                else -> {
+                    loadFragment(ExploreFragment())
+                    true}
+            }
+        }
+    }
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container,fragment)
+        transaction.commit()
     }
 
     override fun onStart() {
